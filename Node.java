f98906f3a567ff0b79c56d6afe4ad103f41a4e7d
Sadid-E <sethun20@stuy.edu>
2021-01-06 23:54:28 -0500

    Added the get/set methods

--- Node.java
+++ Node.java
@@ -7,4 +7,28 @@ public class Node{
  private String data;
  private Node next,prev;
 
+ public String getData() {
+   return data;
+ }
+
+ public Node getNext() {
+   return next;
+ }
+
+ public Node getPrev() {
+   return prev;
+ }
+
+ public void setData(String set) {
+   data = set;
+ }
+
+ public void setNext(Node set) {
+   next = set;
+ }
+
+ public void getPrev(Node set) {
+   prev = set;
+ }
+
 }
