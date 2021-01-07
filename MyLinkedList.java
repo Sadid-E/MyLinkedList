dc50df94dae961feaa77f37f2bdc402500ec7bdd
Sadid-E <sethun20@stuy.edu>
2021-01-07 00:19:22 -0500

    Added constructor

---
+++ MyLinkedList.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class MyLinkedList{
+
+ private int size;
+ private Node start,end;
+
+ public MyLinkedList(){
+   LinkedList<String> list = new LinkedList<String>();
+ }
+
+ // public int size();
+ //
+ // public boolean add(String value);
+ //
+ // public void add(int index, String value);
+ //
+ // public String get(int index);
+ //
+ // public String set(int index, String value);
+ //
+ // public String toString();
+
+}
